d5f0d45e66c18e204891471b7f32f1623f40454d
Henry Lee <gbvrcx@gmail.com>
2020-02-25 22:29:08 +0900

    Installing SynthRL using pip

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # SynthRL [ˈsɪnθrɪl]
+
+## Requirements
+Currently, there are no required libraries and programs.
+
+## Install SyntrhRL
+Clone this repository via SSH
+```
+$ git clone git@github.com:kupl/SynthRL-dev.git
+```
+OR via HTTPS
+```
+$ git clone https://github.com/kupl/SynthRL-dev.git
+```
+Move to project directory and install using pip
+```
+$ cd SynthRL-dev
+$ sudo pip install .
+```
+
+## Uninstall SyntrhRL
+Uninstall using pip
+```
+$ sudo pip uninstall synthrl
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup_params = dict(
+  name='SynthRL',
+  version='0.0',
+  description='Program Synthesizer using Reinforcement Learning',
+  url='https://github.com/kupl/SynthRL-dev',
+  author='Software Analysis Labrotory at Korea University',
+  author_email='noemail@no.email',
+  packages=find_packages(exclude=['example']),
+  setup_requires=[    # required library for setup.py itself
+  ], 
+  install_requires=[  # required library for SynthRL
+    'lark-parser'
+  ], 
+  dependency_links=[  # libraries cannot be found on pip
+  ],
+)
+
+if __name__ == '__main__':
+  setup(**setup_params)
