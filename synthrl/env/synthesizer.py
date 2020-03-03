026743c9c32abba0c8d32121853b958bbcb159c9
Henry Lee <gbvrcx@gmail.com>
2020-03-03 18:27:48 +0900

    Implement environment

--- synthrl/env/synthesizer.py
+++ synthrl/env/synthesizer.py
@@ -1,11 +1,41 @@
+from collections import defaultdict
+
 from synthrl.env.environment import Environment
 
 class SynthesizerEnvironment(Environment):
-  def __init__(self, ioset=[]):
-    pass
+  def __init__(self, program=None, ioset=[]):
+    self.orig_program = program.copy()
+    self.ioset = ioset
+
+    self.reset()
+
+  def reset(self):
+    self.program = self.orig_program.copy()
+    self.__update_space()
+    return self.state, 0, False
+
+  def __update_space(self):
+    self.node, self.space = self.program.production_space()
+  
+  @property
+  def state(self):
+    return self.program.copy(), self.ioset
+
+  def action_space(self):
+    return self.space
 
   def step(self, action=None):
-    pass
+    if action not in self.space:
+      raise ValueError('Action({}) is not valid.'.format(action))
+    self.node.production(action)
+    self.__update_space()
+    if len(self.space) == 0:
+      for i, o in self.ioset:
+        mem = defaultdict(list)
+        mem['v0'] = i[0]
+        mem['v1'] = i[1]
+        if o != self.program.interprete(mem)['v19']:
+          return self.state, 0, True
+      return self.state, 1, True
+    return self.state, 0, False
   
-  def reset(self):
-    pass
--- synthrl/env/verifier.py
+++ synthrl/env/verifier.py
@@ -1,11 +1,41 @@
+from collections import defaultdict
+
 from synthrl.env.environment import Environment
 
 class VerifierEnvironment(Environment):
-  def __init__(self, ioset=[], cand=None):
-    pass
+  def __init__(self, program=None, ioset=[], cand=None):
+    self.orig_program = program.copy()
+    self.ioset = ioset
+    self.cand = cand.copy()
+
+    self.reset()
+
+  def reset(self):
+    self.program = self.orig_program.copy()
+    self.__update_space()
+    return self.state, 0, False
+
+  def __update_space(self):
+    self.node, self.space = self.program.production_space()
+
+  @property
+  def state(self):
+    return self.program.copy(), self.ioset, self.cand.copy()
+
+  def action_space(self):
+    return self.space
 
   def step(self, action=None):
-    pass
-  
-  def reset(self):
-    pass
+    if action not in self.space:
+      raise ValueError('Action({}) is not valid.'.format(action))
+    self.node.production(action)
+    self.__update_space()
+    if len(self.space) == 0:
+      for i, o in self.ioset:
+        mem = defaultdict(list)
+        mem['v0'] = i[0]
+        mem['v1'] = i[1]
+        if o != self.program.interprete(mem)['v19']:
+          return self.state, 0, True
+      return self.state, 1, True
+    return self.state, 0, False
