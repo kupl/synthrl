1719203b24e684e68f23b996e9683d234ad357a2
Henry Lee <gbvrcx@gmail.com>
2020-03-23 22:53:32 +0900

    Test script for synthesize

--- example/test_synthesizer.py
+++ example/test_synthesizer.py
@@ -1,6 +1,9 @@
 from synthrl.agent import RandomAgent
+from synthrl.example.synthesize import synthesize_from_oracle
 from synthrl.language import ListLanguage
-from synthrl.wrapper import Synthesizer
+from synthrl.testing import random_testing
+from synthrl.value import Integer
+from synthrl.value import IntList
 
 oracle = lambda l, i: sorted(l)[i - 1]
 ioset = [
@@ -8,5 +11,5 @@ ioset = [
   (([5, 1, 2], 3), 5)
 ]
 
-synthesizer = Synthesizer(dsl=ListLanguage, synth=RandomAgent(), alt=RandomAgent())
-synthesizer.synthesize_from_oracle(io=ioset, oracle=oracle)
+program = synthesize_from_oracle(dsl=ListLanguage, synthesizer=RandomAgent(), verifier=RandomAgent(), oracle=oracle, ioset=ioset, budget='1m', testing=random_testing, testing_opt={'input_types': (IntList, Integer), 'budget': '10s'})
+program.pretty_print()
