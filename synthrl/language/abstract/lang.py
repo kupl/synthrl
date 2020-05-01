0f40a7f62752ee39c9983f558743a74bdc236c47
Henry Lee <gbvrcx@gmail.com>
2020-05-01 19:06:45 +0900

    Defining abstract structure

---
+++ synthrl/language/abstract/lang.py
@@ -0,0 +1,36 @@
+from typing import Any
+from typing import List
+from typing import Dict
+
+from synthrl.utils.decoratorutils import classproperty
+
+class Tree:
+  def __init__(self, data:str, children:dict, parent:'Tree'=None) -> None:
+    self.data = data
+    self.children = children
+    self.parent = parent
+    
+  def production_space(self) -> List[str]:
+    # returns 
+    raise NotImplementedError
+
+  def interprete(self, *args, **kwargs) -> Any:
+    # gets needed information
+    # returns an executed result of program
+    raise NotImplementedError
+
+  def pretty_print(self, file=None, *args, **kwargs) -> None:
+    # print a tree node
+    raise NotImplementedError
+
+  @classproperty
+  @classmethod
+  def tokens(cls) -> List[str]:
+    # returns a list of tokens
+    raise NotImplementedError
+
+  @property
+  def spec(self) -> Dict[str, Any]:
+    # returns a dictionary that contains all information to create hole
+    raise NotImplementedError
+  
--- synthrl/language/listlang/lang.py
+++ synthrl/language/listlang/lang.py
@@ -1,7 +1,9 @@
 # grammar for ListLang
-# L -> I; I; I; ... I;        # seq: T sequences
-# I -> x_i <- F               # assign: for new x_i
-# F -> MAP AUOP V             # map
+# L -> x_1 <- I;
+#      x_2 <- I;
+#      ...;
+#      x_T <- I;              # seq: T sequences
+# I -> MAP AUOP V             # map
 #    | FILTER BUOP V          # filter
 #    | COUNT BUOP V           # count
 #    | SCANL1 ABOP V          # scanl1
@@ -16,6 +18,7 @@
 #    | TAKE V V               # take
 #    | DROP V V               # drop
 #    | ACCESS V V             # access
+#    | NOP                    # nop
 # V -> a_1 | a_2              # inputs
 #    | x_1 | x_2 | ... | x_T  # variables
 # AUOP -> +1 | -1 | *2 | /2 | *(-1) | **2 | *3 | /3 | *4 | /4
