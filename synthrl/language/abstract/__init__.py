d73383794628bb1bd28ef3c84f3f6bd852addac5
Henry Lee <gbvrcx@gmail.com>
2020-05-04 01:14:24 +0900

    Classes updated

---
+++ synthrl/language/abstract/__init__.py
@@ -0,0 +1,3 @@
+from synthrl.language.abstract.lang import Node
+from synthrl.language.abstract.lang import Tree
+from synthrl.language.abstract.lang import UndefinedSemantics
--- synthrl/language/abstract/lang.py
+++ synthrl/language/abstract/lang.py
@@ -1,14 +1,26 @@
 from synthrl.utils.decoratorutils import classproperty
 
-# very outer class that represent root node
-class Tree:
+# Exception to handle when semantic is not defined
+class UndefinedSemantics(Exception):
   def __init__(self, *args, **kwargs):
-    pass
+    super(UndefinedSemantics, self).__init__(*args, **kwargs)
 
+# Abstract class that very first symbol which represent root node should implement
+class Tree:
+
+  __slots__ = ['hole']
+
+  def __init__(self, *args, **kwargs):
+    self.hole = None
+
+  @property
   def production_space(self):
     raise NotImplementedError
 
   def production(self, rule):
+    raise NotImplementedError
+
+  def interprete(self, inputs):
     raise NotImplementedError
 
   def pretty_print(self, file=None):
@@ -19,13 +31,21 @@ class Tree:
   def tokens(cls):
     raise NotImplementedError
 
+# Abstract class that each non-terminal symbols should implement
 class Node:
+
+  __slots__ = [
+    'children',
+    'data',
+    'parent',
+  ]
+
   def __init__(self, data='hole', children={}, parent=None):
     self.data = data
     self.children = children
     self.parent = parent
     
-  def production_space(self):
+  def production_space(self, *args, **kwargs):
     # returns a list of possible production rules
     raise NotImplementedError
 
