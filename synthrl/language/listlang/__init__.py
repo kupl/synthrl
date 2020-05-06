c59c8df4c7a309be673d9a45bb41f4af75f43102
Henry Lee <gbvrcx@gmail.com>
2020-05-06 17:22:56 +0900

    Interface of OracleSampler

--- synthrl/language/listlang/__init__.py
+++ synthrl/language/listlang/__init__.py
@@ -1,1 +1,2 @@
 from synthrl.language.listlang.lang import ListLang
+from synthrl.language.listlang.oracle import OracleSampler
---
+++ synthrl/language/listlang/oracle.py
@@ -0,0 +1,6 @@
+from synthrl.utils.trainutils import Dataset
+
+def OracleSampler():
+  # TODO
+  # this should return synthrl.utils.trainutils.Dataset object
+  raise NotImplementedError
