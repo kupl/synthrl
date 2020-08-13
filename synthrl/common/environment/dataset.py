6eb8c4bfc846bddf2e9125c5c03eb18c2d89380f
Henry Lee <gbvrcx@gmail.com>
2020-08-14 00:41:27 +0900

    Copy dataset

--- synthrl/common/environment/dataset.py
+++ synthrl/common/environment/dataset.py
@@ -1,3 +1,5 @@
+from io import StringIO
+
 from synthrl.common.environment.ioset import IOSet
 
 class Element:
@@ -6,11 +8,49 @@ class Element:
     self.oracle = oracle
     self.ioset = IOSet(ioset)
 
+  def __getitem__(self, idx):
+    if idx == 0:
+      return self.oracle
+    elif idx == 1:
+      return self.ioset
+    else:
+      raise IndexError('Index 0 for oracle, and index 1 for ioset.')
+
 class Dataset:
 
-  def __init__(self, oracle, ioset):
-    self.oracle = oracle
-    self.ioset = IOSet(ioset)
+  def __init__(self):
+    self.elements = []
+
+  def __getitem__(self, idx):
+    return self.elements[idx]
+
+  def add(self, program, ioset):
+    self.elements.append(Element(program, ioset))
+
+  def __repr__(self):
+
+    stream = StringIO()
+
+    for pgm, ioset in self.elements:
+      print('--program--', file=stream)
+      pgm.pretty_print(file=stream)
+      
+      print('--io set--', file=stream)
+      for pair in ioset:
+        print(pair, file=stream)
+
+      print(file=stream)
+
+    string = stream.getvalue()
+    stream.close()
+    return string
+
+  def __str__(self):
+    return repr(self)
+
+  @property
+  def length(self):
+    return len(self.elements)
 
   def to_json(self, json):
     pass
--- synthrl/common/environment/ioset.py
+++ synthrl/common/environment/ioset.py
@@ -6,8 +6,11 @@ class IOSet:
     self.ioset = list(ioset)
     self.n_example = len(self.ioset)
 
+  def __len__(self):
+    return self.n_example
+
   def get_rand(self, n):
-    indices = np.random.permutation(self.n_example)[:n]
+    indices = (np.random.permutation(self.n_example).tolist() * (n // self.n_example + 1))[:n]
     return [self.ioset[i] for i in indices]
     
   def __getitem__(self, idx):
