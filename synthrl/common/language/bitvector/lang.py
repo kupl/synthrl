43ff7d677e6e7eb030cf21a1e88876ed86462222
Henry Lee <gbvrcx@gmail.com>
2020-08-31 20:19:54 +0900

    Bug fix.
    TOKENS should include HOLE as token.

--- synthrl/common/language/bitvector/lang.py
+++ synthrl/common/language/bitvector/lang.py
@@ -118,7 +118,7 @@ class BitVectorLang(Program):
   @classproperty
   @classmethod
   def TOKENS(cls):
-    return sorted(ExprNode.TOKENS + BOPNode.TOKENS + ConstNode.TOKENS + ParamNode.TOKENS)
+    return set(ExprNode.TOKENS + BOPNode.TOKENS + ConstNode.TOKENS + ParamNode.TOKENS).union({HOLE})
 
   @classmethod
   def tokens2prog(cls, tokens = []):
